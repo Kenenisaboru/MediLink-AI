21ff6322c99d76c532cfd319ac4511e8c59a41de
Kenenisaboru <kenenisaboru998@gmail.com>
2026-08-03 07:44:11 -0700

    Update DashboardHeader component

--- frontend/src/components/DashboardHeader.tsx
+++ frontend/src/components/DashboardHeader.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React, { useState } from 'react';
-import useRouterPath from 'next/navigation';
 import { useRouter, usePathname } from 'next/navigation';
 import {
   Activity,
